58abeb498773b34b8668912409a1dd3dce996473
bhavesh annam <bhaveshannam@gmail.com>
2021-04-29 14:46:00 +0530

    Add Cors

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 const path = require("path");
 
+const cors = require("cors");
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
@@ -14,6 +16,16 @@ const connectDB = require("./config/db");
 connectDB();
 
 
+//Specifying from which all requests cors should be allowed
+
+const corsOptions = {
+    origin:process.env.ALLOW_CLIENTS.split(",")
+
+}
+
+app.use(cors(corsOptions));
+
+
 app.set("views",path.join(__dirname,"/views"));
 app.set("view engine","ejs");
 
@@ -21,9 +33,7 @@ app.set("view engine","ejs");
 app.use("/api/files",require("./routes/files"));
 app.use("/files",require("./routes/show"));
 app.use("/files/download",require("./routes/download"));
-app.get("/",(req,res)=>{
-    res.send("hey I listen to you");
-})
+
 
 app.listen(PORT , ()=>{
     console.log(`Listeneing on PORT: ${PORT}`);
